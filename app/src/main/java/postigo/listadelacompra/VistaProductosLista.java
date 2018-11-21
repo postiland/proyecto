99e46462e9b49412629d42196c9497a2b0be4359
DESKTOP-JTIHTFC\Postiland <postiland@gmail.com>
2018-11-21 20:33:02 +0100

    Añadida funcionalidad boton añadir producto.

--- app/src/main/java/postigo/listadelacompra/VistaProductosLista.java
+++ app/src/main/java/postigo/listadelacompra/VistaProductosLista.java
@@ -32,7 +32,7 @@ import cz.msebera.android.httpclient.Header;
 
 public class VistaProductosLista extends AppCompatActivity implements View.OnClickListener{
 
-    private String datosProducto_id_producto;
+    private String datosLista_id_lista;
 
     private Button btn_anadir_producto;
 
@@ -45,7 +45,7 @@ public class VistaProductosLista extends AppCompatActivity implements View.OnCli
 
     public static final String URL_COGER_PRODUCTOS = "http://antoniopostigo.es/Slim2-ok/api/id_lista/obtener/productos";
 
-    public static final String URL_CREAR_PRODUCTO = "http://antoniopostigo.es/Slim2-ok/api/anadir/producto";
+    public static final String URL_CREAR_PRODUCTO = "http://antoniopostigo.es/Slim2-ok/api/anadir/producto/lista";
 
     public static final String URL_ELIMINAR_RELACION_LISTA_PRODUCTO = "http://antoniopostigo.es/Slim2-ok/api/anadir/producto";
 
@@ -54,7 +54,7 @@ public class VistaProductosLista extends AppCompatActivity implements View.OnCli
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vista_productos_lista);
 
-        datosProducto_id_producto= getIntent().getStringExtra("ID_LISTA");
+        datosLista_id_lista= getIntent().getStringExtra("ID_LISTA");
 
         lista_productos=(ListView) findViewById(R.id.ltv_productos);
 
@@ -64,7 +64,7 @@ public class VistaProductosLista extends AppCompatActivity implements View.OnCli
         btn_limpiar_lista=(Button) findViewById(R.id.btn_limpiar_lista);
         btn_limpiar_lista.setOnClickListener(this);
 
-        cogerProductos(datosProducto_id_producto);
+        cogerProductos(datosLista_id_lista);
 
         productos_lista=new ArrayList<Producto>();
         myAdapter = new ArrayAdapter<Producto>(this, android.R.layout.simple_list_item_1, productos_lista);
@@ -74,8 +74,6 @@ public class VistaProductosLista extends AppCompatActivity implements View.OnCli
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Usuario usuario = (Usuario) lista_productos.getAdapter().getItem(position);
-
-                //Toast.makeText(getApplicationContext(), "->"+lista.getId_lista(), Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -175,15 +173,27 @@ public class VistaProductosLista extends AppCompatActivity implements View.OnCli
         TextView txv_pro_cant = new TextView(this);
         txv_pro_cant.setText("Cantidad:");
 
+        final EditText edt_pro_prec = new EditText(this);
+        edt_pro_prec.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+        edt_pro_prec.setText("0");
+        TextView txv_pro_prec = new TextView(this);
+        txv_pro_cant.setText("Precio:");
+
         LinearLayout.LayoutParams tv1Params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         tv1Params.bottomMargin = 5;
         LinearLayout.LayoutParams tv2Params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         tv2Params.topMargin = 5;
         tv2Params.bottomMargin = 5;
+        LinearLayout.LayoutParams tv3Params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        tv3Params.topMargin = 5;
+        tv3Params.bottomMargin = 5;
+
         layout.addView(txv_nom_pro, tv1Params);
         layout.addView(edt_nom_pro, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
         layout.addView(txv_pro_cant, tv2Params);
         layout.addView(edt_pro_cant, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        layout.addView(txv_pro_prec, tv3Params);
+        layout.addView(edt_pro_prec, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
 
         alertDialogBuilder.setView(layout);
         alertDialogBuilder.setTitle("EDITAR LISTA");
@@ -206,16 +216,20 @@ public class VistaProductosLista extends AppCompatActivity implements View.OnCli
                 try {
                     Toast.makeText(getApplicationContext(), "->" + edt_nom_pro.getText().toString(), Toast.LENGTH_SHORT).show();
                     int cant_pro = 1;
+                    double prec_pro = 0;
 
                     if (!edt_nom_pro.getText().toString().isEmpty()){
                         if (!edt_pro_cant.getText().toString().isEmpty()){
-                            cant_pro = Integer.parseInt(edt_nom_pro.getText().toString());
+                            cant_pro = Integer.parseInt(edt_pro_cant.getText().toString());
                         }
-                        crearProducto(edt_nom_pro.getText().toString(), cant_pro);
+                        if (!edt_pro_prec.getText().toString().isEmpty()){
+                            prec_pro = Double.parseDouble(edt_pro_prec.getText().toString());
+                        }
+                        crearProducto(edt_nom_pro.getText().toString(), cant_pro, prec_pro);
                     }
 
                 }catch (Exception e){
-                    Toast.makeText(getApplicationContext(), "ERROR AL DAR OK", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "ERROR AL DAR OK"+"->", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -229,12 +243,14 @@ public class VistaProductosLista extends AppCompatActivity implements View.OnCli
         }
     }
 
-    private void crearProducto(String nombre_producto, int cantidad_producto) {
+    private void crearProducto(String nombre_producto, int cantidad_producto, double precio_producto) {
         final ProgressDialog progreso = new ProgressDialog(getApplicationContext());
         AsyncHttpClient client = new AsyncHttpClient();
         RequestParams envio_nueva_lista = new RequestParams();
         envio_nueva_lista.put("nombre", nombre_producto);
         envio_nueva_lista.put("cantidad", cantidad_producto);
+        envio_nueva_lista.put("precio", precio_producto);
+        envio_nueva_lista.put("id_lista", datosLista_id_lista);
 
         client.post(URL_CREAR_PRODUCTO, envio_nueva_lista, new JsonHttpResponseHandler(){
             @Override
